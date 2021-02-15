6fd1fbf3ff9432ab84af98ef7caef5e6cd32015d
Rutvij Sathe <rut26sathe@gmail.com>
2021-02-15 16:56:58 +0530

    StyledTitle added

--- src/ui/components/attributes.tsx
+++ src/ui/components/attributes.tsx
@@ -1,7 +1,18 @@
 import React, { FC } from "react";
+import styled from "styled-components";
+
+export const StyledTitle = styled.div`
+    color: ${(props) => props.theme.colors.secondary};
+    font-size: 30px;
+    margin: 20px;
+`;
 
 const Attributes: FC = () => {
-    return <div>Attribute Rendered!</div>;
+    return (
+        <div>
+            <StyledTitle> Attribute Rendered!</StyledTitle>
+        </div>
+    );
 };
 
 export default Attributes;
